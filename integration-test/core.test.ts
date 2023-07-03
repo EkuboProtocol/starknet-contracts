62757b3c91d3ed2010f2a7a2ae15becc09293736
Moody Salem <moody.salem@gmail.com>
2023-07-02 23:29:01 -0400

    commit just integ test file

--- integration-test/core.test.ts
+++ integration-test/core.test.ts
@@ -1,57 +1,67 @@
-import { ChildProcessByStdio, ChildProcessWithoutNullStreams, spawn, SpawnOptionsWithStdioTuple } from 'child_process'
-import fs from "fs";
+import { ChildProcessWithoutNullStreams, spawn } from "child_process";
 
-import {
-    defaultProvider,
-    ec,
-    json,
-    SequencerProvider,
-    Account,
-    Provider,
-} from "starknet";
+import { Account, RpcProvider, Contract, ContractFactory } from "starknet";
 
-describe('core tests', () => {
-    let starknetProcess: ChildProcessWithoutNullStreams
-    let rpcUrl: string
-    let accounts: { address: string, privateKey: string, publicKey: string }[]
-    let provider: SequencerProvider
+describe("core tests", () => {
+  let starknetProcess: ChildProcessWithoutNullStreams;
+  let rpcUrl: string;
+  let accounts: Account[];
+  let provider: RpcProvider;
 
-    beforeAll(() => {
-        starknetProcess = spawn('katana', ['--seed', '0'])
+  beforeAll(() => {
+    starknetProcess = spawn("katana", ["--seed", "0"]);
 
-        return new Promise((resolve) => {
-            starknetProcess.stdout.once('data', (data) => {
-                let str = data.toString('utf8');
-                // console.log(str);
-                rpcUrl = /(http:\/\/[\w\.:\d]+)/g.exec(str)?.[1]
-                rpcUrl = rpcUrl.replace(/0\.0\.0\.0/, 'localhost')
-                // rpcUrl = 'http://localhost:5050'
-                provider = new SequencerProvider({ baseUrl: rpcUrl, gatewayUrl: rpcUrl })
-                accounts = [...str.matchAll(/\|\s+Account Address\s+\|\s+(0x[a-f0-9]+)\s+\|\s+Private key\s+\|\s+(0x[a-f0-9]+)\s+\|\s+Public key\s+\|\s+(0x[a-f0-9]+)/gi)]
-                    .map(match => ({ address: match[1], privateKey: match[2], publicKey: match[3] }))
-                resolve(null)
-            })
-        })
-    })
+    return new Promise((resolve) => {
+      starknetProcess.stdout.once("data", (data) => {
+        let str = data.toString("utf8");
 
-    beforeEach(async () => {
-        console.log(await provider.getBlock('latest'))
-        console.log(await provider.getChainId())
-    })
+        rpcUrl = /(http:\/\/[\w\.:\d]+)/g
+          .exec(str)?.[1]
+          ?.replace(/0\.0\.0\.0/, "127.0.0.1");
 
-    it('works', () => {
-
-    })
-
-    afterAll(async () => {
-        console.log('killing devnet')
-
-        return new Promise(resolve => {
-            starknetProcess.on('close', () => {
-                console.log('closed');
-                resolve(null);
-            });
-            starknetProcess.kill()
+        provider = new RpcProvider({
+          nodeUrl: rpcUrl,
         });
-    })
-})
+
+        accounts = [
+          ...str.matchAll(
+            /\|\s+Account Address\s+\|\s+(0x[a-f0-9]+)\s+\|\s+Private key\s+\|\s+(0x[a-f0-9]+)\s+\|\s+Public key\s+\|\s+(0x[a-f0-9]+)/gi
+          ),
+        ]
+          .map((match) => ({
+            address: match[1],
+            privateKey: match[2],
+            publicKey: match[3],
+          }))
+          .map(
+            ({ address, privateKey }) =>
+              new Account(provider, address, privateKey)
+          );
+
+        resolve(null);
+      });
+    });
+  });
+
+  let factory: ContractFactory;
+  let core: Contract;
+
+  beforeEach(async () => {
+    factory = new ContractFactory({}, contract_address, accounts[0]);
+    core = await factory.deploy();
+  });
+
+  it("works", () => {});
+
+  afterAll(async () => {
+    console.log("killing devnet");
+
+    return new Promise((resolve) => {
+      starknetProcess.on("close", () => {
+        console.log("closed");
+        resolve(null);
+      });
+      starknetProcess.kill();
+    });
+  });
+});
