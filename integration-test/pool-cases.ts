1d164df1ec4f53fa84b25b77778d4d57707de813
Moody Salem <moody.salem@gmail.com>
2023-08-16 16:59:44 +0200

    fix cases so they can run

--- integration-test/pool-cases.ts
+++ integration-test/pool-cases.ts
@@ -1,7 +1,12 @@
 import { MAX_TICK, MIN_TICK } from "./constants";
 
 const THIRTY_BIPS_FEE = (2n ** 128n * 30n) / 10000n;
-const TICK_SPACING_60_BIPS = 5982n;
+const SIXTY_BIPS_TICK_SPACING = 5982n;
+
+function nearest(tick: bigint, spacing: bigint): bigint {
+  return (tick / spacing) * spacing;
+}
+
 export const POOL_CASES: Array<{
   name: string;
   pool: {
@@ -21,18 +26,24 @@ export const POOL_CASES: Array<{
     name: "full range liquidity, starting at price 1, tick_spacing=0.6%, fee=0.3%",
     pool: {
       startingTick: 0n,
-      tickSpacing: TICK_SPACING_60_BIPS,
+      tickSpacing: SIXTY_BIPS_TICK_SPACING,
       fee: THIRTY_BIPS_FEE,
     },
     positions: [
-      { bounds: { lower: MIN_TICK, upper: MAX_TICK }, liquidity: 10000n },
+      {
+        bounds: {
+          lower: nearest(MIN_TICK, SIXTY_BIPS_TICK_SPACING),
+          upper: nearest(MAX_TICK, SIXTY_BIPS_TICK_SPACING),
+        },
+        liquidity: 10000n,
+      },
     ],
   },
   {
     name: "no liquidity, starting at price 1, tick_spacing=0.6%, fee=0.3%",
     pool: {
       startingTick: 0n,
-      tickSpacing: TICK_SPACING_60_BIPS,
+      tickSpacing: SIXTY_BIPS_TICK_SPACING,
       fee: THIRTY_BIPS_FEE,
     },
     positions: [],
