f5379d253f43bfc443ed78e3748d06b9f500836e
Moody Salem <moody.salem@gmail.com>
2023-08-16 16:56:18 +0200

    more realistic practical cases

--- integration-test/pool-cases.ts
+++ integration-test/pool-cases.ts
@@ -1,6 +1,7 @@
 import { MAX_TICK, MIN_TICK } from "./constants";
 
-const THIRTY_BIPS = (2n ** 128n * 30n) / 10000n;
+const THIRTY_BIPS_FEE = (2n ** 128n * 30n) / 10000n;
+const TICK_SPACING_60_BIPS = 5982n;
 export const POOL_CASES: Array<{
   name: string;
   pool: {
@@ -17,19 +18,23 @@ export const POOL_CASES: Array<{
   }[];
 }> = [
   {
-    name: "no liquidity, starting at price 1, tick_spacing==1, fee=0.003",
-    pool: { startingTick: 0n, tickSpacing: 1n, fee: THIRTY_BIPS },
-    positions: [],
-  },
-  {
-    name: "full range liquidity, starting at price 1, tick_spacing==1, fee=0.003",
+    name: "full range liquidity, starting at price 1, tick_spacing=0.6%, fee=0.3%",
     pool: {
       startingTick: 0n,
-      tickSpacing: 1n,
-      fee: THIRTY_BIPS,
+      tickSpacing: TICK_SPACING_60_BIPS,
+      fee: THIRTY_BIPS_FEE,
     },
     positions: [
       { bounds: { lower: MIN_TICK, upper: MAX_TICK }, liquidity: 10000n },
     ],
+  },
+  {
+    name: "no liquidity, starting at price 1, tick_spacing=0.6%, fee=0.3%",
+    pool: {
+      startingTick: 0n,
+      tickSpacing: TICK_SPACING_60_BIPS,
+      fee: THIRTY_BIPS_FEE,
+    },
+    positions: [],
   },
 ];
