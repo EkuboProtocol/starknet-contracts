8a475122e83feee137a328ef1b98e0bbbc316935
Moody Salem <moody.salem@gmail.com>
2023-09-27 10:55:27 -0400

    fix the declare file

---
+++ scripts/declare-all.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# Function to print usage and exit
+print_usage_and_exit() {
+    echo "Usage: $0 --network {goerli-1,mainnet}"
+    exit 1
+}
+
+# Ensure there are exactly two arguments
+if [ "$#" -ne 2 ]; then
+    print_usage_and_exit
+fi
+
+# Parse the arguments
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --network)
+            NETWORK="$2"
+            shift
+            ;;
+        *)
+            echo "Unknown parameter passed: $1"
+            print_usage_and_exit
+            ;;
+    esac
+    shift
+done
+
+# Ensure network is valid
+if [ "$NETWORK" != "goerli-1" -a "$NETWORK" != "mainnet" ]; then
+    echo "Invalid network: $NETWORK"
+    print_usage_and_exit
+fi
+
+
+scarb build
+
+declare_class_hash() {
+    local class_name=$1
+    starkli declare --network "$NETWORK" --keystore-password "$STARKNET_KEYSTORE_PASSWORD" --casm-file  "target/dev/ekubo_${class_name}.casm.json" "target/dev/ekubo_${class_name}.sierra.json"
+}
+
+echo "Declaring core"
+CORE_CLASS_HASH=$(declare_class_hash Core)
+echo "Declaring positions"
+POSITIONS_CLASS_HASH=$(declare_class_hash Positions)
+echo "Declaring NFT"
+NFT_CLASS_HASH=$(declare_class_hash EnumerableOwnedNFT)
+
+echo "Declared core @ $CORE_CLASS_HASH"
+echo "Declared positions @ $POSITIONS_CLASS_HASH"
+echo "Declared nft @ $NFT_CLASS_HASH"
+
