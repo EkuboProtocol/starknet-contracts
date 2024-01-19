39072372d1089b99b6ebcac0dc8d224e9957440b
Moody Salem <moody.salem@gmail.com>
2024-01-19 14:36:18 -0500

    make it more readable

--- scripts/codegen/gen-exp-fractional.ts
+++ scripts/codegen/gen-exp-fractional.ts
@@ -1,17 +1,25 @@
 import {Decimal} from 'decimal.js'
 
-function genTickMath({
-                         base,
-                         outputFixedPointRadix,
-                         maxRatio,
-                     }: { base: Decimal, outputFixedPointRadix: number, maxRatio: Decimal }) {
+Decimal.config({precision: 1000, toExpPos: 999})
+
+function genExpFractionalInput({
+                                   base: rawBase,
+                                   maxInput,
+                                   inputFixedPointRadix,
+                                   outputFixedPointRadix,
+                               }: {
+    base: Decimal,
+    maxInput: Decimal,
+    inputFixedPointRadix: number,
+    outputFixedPointRadix: number,
+}) {
+    const base = rawBase.pow(new Decimal(1).div(new Decimal(2).pow(inputFixedPointRadix)))
     // log base 10 of 2**256 ~= 78, so 250 is plenty for maximum accuracy
-    Decimal.config({precision: 250, toExpPos: 999})
     const QFP = new Decimal(2).pow(outputFixedPointRadix);
 
-    const tickOfMaxRatio = maxRatio.ln().div(base.ln())
+    const maxInputRaw = maxInput.mul(new Decimal(2).pow(inputFixedPointRadix))
 
-    const numIterations = tickOfMaxRatio.ln().div(new Decimal(2).ln()).ceil().toNumber()
+    const numIterations = maxInputRaw.ln().div(new Decimal(2).ln()).ceil().toNumber()
 
     console.log(`// base = ${base.toPrecision(12).toString()}`)
     console.log(`// number of iterations = ${numIterations}`)
@@ -23,7 +31,9 @@ function genTickMath({
     for (let i = 0; i < numIterations; i++) {
         const tickMultiplier = base.pow(new Decimal(2).pow(i));
         const inverse = QFP.div(tickMultiplier).round()
-        const inverseStr = `0x${BigInt(inverse.toString()).toString(16)}`
+        const inverseBigInt = BigInt(inverse.toString());
+        if (inverseBigInt === 0n) break
+        const inverseStr = `0x${inverseBigInt.toString(16)}`
         const tickBit = `0x${(1n << BigInt(i)).toString(16)}`
 
         if (i == 0) {
@@ -42,9 +52,10 @@ function genTickMath({
 `)
 }
 
-genTickMath({
-    base: new Decimal('1.0000000000000000000542101086242752217018420079820249449562765347'),
+genExpFractionalInput({
+    base: Decimal.exp(1),
+    maxInput: new Decimal(2).pow(64),
+    inputFixedPointRadix: 64,
     outputFixedPointRadix: 128,
-    maxRatio: new Decimal(2).pow(128),
 });
 
